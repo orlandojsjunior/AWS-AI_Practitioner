feb5b6be67b296a8a8c089b5498eb49062608ee2
Orlando Junior <orlandojsjunior@hotmail.com>
2025-03-17 18:43:56 -0300

    Atividade-01

---
+++ Atividade-01.py
@@ -0,0 +1,34 @@
+# 1- Programa de Saudação
+
+print("Olá, mundo!")
+
+
+# 2 - Programa que soma dois números
+
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+
+print("A soma de", numero1, "e", numero2, "é:", soma)
+
+
+# 3- Calculadora de Volume
+
+comprimento = 12
+largura = 14
+altura = 20
+volume = comprimento * largura * altura
+
+print("O volume da caixa retangular é:", volume, "cm³")
+
+
+# 4 - Calculadora de Preço Total de Compra
+
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+preco_total = preco_unitario * quantidade
+
+print(f"Produto: {nome_produto}, Preço unitário: R$ {preco_unitario:.2f}, Quantidade: {quantidade}, Preço total: R$ {preco_total:.2f}")
+
+
