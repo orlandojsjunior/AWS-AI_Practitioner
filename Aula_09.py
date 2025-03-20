e14e954d9899f174104a4b346cc69f31862345d7
Orlando Junior <orlandojsjunior@hotmail.com>
2025-03-20 19:31:57 -0300

    ADD - função para calcular desconto em produtos

--- Aula_09.py
+++ Aula_09.py
@@ -21,6 +21,7 @@ gorjeta = calcular_gorjeta(total_conta, porcentagem)
 print(f"Para uma conta de R${total_conta:.2f}, a gorjeta de {porcentagem}% é R${gorjeta:.2f}")
 
 
+#------------------------------------------------------------
 
 def is_palindromo(texto):
     # Remove espaços e converte para minúsculas
@@ -39,3 +40,19 @@ else:
     resposta = "Não"
 
 print(f"'{frase}' é um palíndromo? {resposta}")
+
+#------------------------------------------------------------
+
+def calcular_desconto(preco, percentual_desconto):
+
+    desconto = preco * (percentual_desconto / 100)
+    preco_final = preco - desconto
+    return round(preco_final, 2)
+
+# Input do usuário
+preco_original = float(input("Digite o preço do produto: R$ "))
+desconto = float(input("Digite o percentual de desconto: "))
+
+# Calcula e exibe o resultado
+preco_com_desconto = calcular_desconto(preco_original, desconto)
+print(f"O preço final com {desconto}% de desconto é: R$ {preco_com_desconto:.2f}")
